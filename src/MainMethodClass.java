49d5fa41bf625d5d86025c0d8c4a1b532ed571be
animesh-sahai <animesh@infrrd.ai>
2020-04-28 12:33:49 +0530

    Adding hello3

--- src/MainMethodClass.java
+++ src/MainMethodClass.java
@@ -3,5 +3,6 @@ public class MainMethodClass {
     public static void main (String[] args){
         System.out.println("Hello1");
         System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
